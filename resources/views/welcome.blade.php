2936da1282833e28524f723c18545d780dad9c2e
Miftahul Azzahra <miftahulazzahra988@gmail.com>
2026-05-09 23:43:53 +0700

    feat: Membuat UI header dan menu navigasi utama

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ETC Planet</title>
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+
+    <header class="header-atas">
+
+        <div class="logo-kiri">
+            <h2>ETC <span>Planet</span></h2>
+        </div>
+
+
+        <nav class="menu-tengah">
+            <a href="#" class="menu-aktif">Beranda</a>
+            <a href="#">Program</a>
+            <a href="#">Galeri Reels</a>
+            <a href="#">Tentang Kami</a>
+            <a href="#">Kontak</a>
+        </nav>
+
+
+        <div class="menu-kanan">
+            <a href="#" class="link-masuk">Masuk</a>
+            <a href="#" class="tombol-daftar">Daftar Sekarang</a>
+        </div>
+    </header>
+
+</body>
+
+</html>
