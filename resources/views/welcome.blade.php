dbb7637f2a1413db4127aac75e508cb1f5fd3336
Miftahul Azzahra <miftahulazzahra988@gmail.com>
2026-05-11 02:17:28 +0700

    feat: Menambahkan keseluruhan section Program Kursus Unggulan beserta card programnya

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,7 +40,13 @@
                 interaktif, asik, dan terbukti efektif untuk segala usia.</p>
 
             <div class="grup-tombol-hero">
-                <a href="#" class="tombol-utama">Daftar Sekarang &rarr;</a>
+                <a href="#" class="tombol-utama">Daftar Sekarang <svg width="10" height="10"
+                        viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M7.10208 5.25H0V4.08333H7.10208L3.83542 0.816667L4.66667 0L9.33333 4.66667L4.66667 9.33333L3.83542 8.51667L7.10208 5.25Z"
+                            fill="white" />
+                    </svg>
+                </a>
                 <a href="#" class="tombol-garis">Lihat Program</a>
             </div>
 
@@ -80,7 +86,7 @@
         </div>
 
         <div class="hero-kanan">
-            <img src="{{ asset('images/hero-img.jpeg')}}" alt="Guru mengajar" class="gambar-hero">
+            <img src="{{ asset('images/hero-img.jpeg') }}" alt="Guru mengajar" class="gambar-hero">
 
             <div class="kotak-promo">
                 <div class="ikon-centang"><svg width="22" height="21" viewBox="0 0 22 21" fill="none"
@@ -115,6 +121,138 @@
         <div class="item-baris">
             <h3>98%</h3>
             <p>Kepuasan Siswa</p>
+        </div>
+    </section>
+
+    <!-- Program Kursus Unggulan -->
+    <section class="program-unggulan">
+        <div class="judul-section">
+            <h2>Program Kursus Unggulan</h2>
+            <p>Pilih program yang sesuai dengan kebutuhan dan target pembelajaranmu. Tersedia<br>kelas untuk berbagai
+                usia dan tingkat kemahiran.</p>
+        </div>
+
+        <div class="grid-kartu">
+            <!-- Kartu 1: General English -->
+            <div class="kartu-program">
+                <div class="bungkus-gambar">
+                    <span class="label-kategori-absolut">English</span>
+                    <!-- Pastikan panggil gambar asli: asset('images/general-english.png') -->
+                    <img src="{{ asset('images/pu1-img.jpg') }}" alt="General English">
+                </div>
+                <div class="isi-kartu">
+                    <div class="header-kartu">
+                        <h3>General English</h3>
+                        <span class="badge-usia"><svg width="11" height="11" viewBox="0 0 11 11" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M6.70833 5.10417C6.50417 5.10417 6.3316 5.03368 6.19063 4.89271C6.04965 4.75174 5.97917 4.57917 5.97917 4.375C5.97917 4.17083 6.04965 3.99826 6.19063 3.85729C6.3316 3.71632 6.50417 3.64583 6.70833 3.64583C6.9125 3.64583 7.08507 3.71632 7.22604 3.85729C7.36701 3.99826 7.4375 4.17083 7.4375 4.375C7.4375 4.57917 7.36701 4.75174 7.22604 4.89271C7.08507 5.03368 6.9125 5.10417 6.70833 5.10417ZM3.79167 5.10417C3.5875 5.10417 3.41493 5.03368 3.27396 4.89271C3.13299 4.75174 3.0625 4.57917 3.0625 4.375C3.0625 4.17083 3.13299 3.99826 3.27396 3.85729C3.41493 3.71632 3.5875 3.64583 3.79167 3.64583C3.99583 3.64583 4.1684 3.71632 4.30937 3.85729C4.45035 3.99826 4.52083 4.17083 4.52083 4.375C4.52083 4.57917 4.45035 4.75174 4.30937 4.89271C4.1684 5.03368 3.99583 5.10417 3.79167 5.10417ZM5.25 8.16667C4.66667 8.16667 4.13924 8.00625 3.66771 7.68542C3.19618 7.36458 2.84861 6.94167 2.625 6.41667H7.875C7.65139 6.94167 7.30382 7.36458 6.83229 7.68542C6.36076 8.00625 5.83333 8.16667 5.25 8.16667ZM5.25 10.5C4.52083 10.5 3.83785 10.3615 3.20104 10.0844C2.56424 9.80729 2.01007 9.43299 1.53854 8.96146C1.06701 8.48993 0.692708 7.93576 0.415625 7.29896C0.138542 6.66215 0 5.97917 0 5.25C0 4.52083 0.138542 3.83785 0.415625 3.20104C0.692708 2.56424 1.06701 2.01007 1.53854 1.53854C2.01007 1.06701 2.56424 0.692708 3.20104 0.415625C3.83785 0.138542 4.52083 0 5.25 0C5.97917 0 6.66215 0.138542 7.29896 0.415625C7.93576 0.692708 8.48993 1.06701 8.96146 1.53854C9.43299 2.01007 9.80729 2.56424 10.0844 3.20104C10.3615 3.83785 10.5 4.52083 10.5 5.25C10.5 5.97917 10.3615 6.66215 10.0844 7.29896C9.80729 7.93576 9.43299 8.48993 8.96146 8.96146C8.48993 9.43299 7.93576 9.80729 7.29896 10.0844C6.66215 10.3615 5.97917 10.5 5.25 10.5ZM5.25 9.33333C6.37778 9.33333 7.34028 8.93472 8.1375 8.1375C8.93472 7.34028 9.33333 6.37778 9.33333 5.25C9.33333 4.12222 8.93472 3.15972 8.1375 2.3625C7.34028 1.56528 6.37778 1.16667 5.25 1.16667C5.19167 1.16667 5.13333 1.16667 5.075 1.16667C5.01667 1.16667 4.95833 1.17639 4.9 1.19583C4.84167 1.25417 4.80278 1.31736 4.78333 1.38542C4.76389 1.45347 4.75417 1.52639 4.75417 1.60417C4.75417 1.80833 4.82465 1.9809 4.96562 2.12188C5.1066 2.26285 5.27917 2.33333 5.48333 2.33333C5.57083 2.33333 5.65104 2.31875 5.72396 2.28958C5.79688 2.26042 5.87222 2.24583 5.95 2.24583C6.06667 2.24583 6.16389 2.28958 6.24167 2.37708C6.31944 2.46458 6.35833 2.56667 6.35833 2.68333C6.35833 2.90694 6.25382 3.05035 6.04479 3.11354C5.83576 3.17674 5.64861 3.20833 5.48333 3.20833C5.04583 3.20833 4.6691 3.05035 4.35313 2.73438C4.03715 2.4184 3.87917 2.04167 3.87917 1.60417C3.87917 1.575 3.87917 1.54583 3.87917 1.51667C3.87917 1.4875 3.88403 1.44861 3.89375 1.4C3.08681 1.69167 2.43056 2.18264 1.925 2.87292C1.41944 3.56319 1.16667 4.35556 1.16667 5.25C1.16667 6.37778 1.56528 7.34028 2.3625 8.1375C3.15972 8.93472 4.12222 9.33333 5.25 9.33333Z"
+                                    fill="#27171C" />
+                            </svg>
+                            Kids/Teens</span>
+                    </div>
+                    <p>Program bahasa Inggris umum untuk anak dan remaja. Fokus pada percakapan dan tata bahasa dasar.
+                    </p>
+                    <hr class="garis-kartu">
+                    <div class="footer-kartu">
+                        <div class="harga-mulai">
+                            <span class="label-mulai">MULAI DARI</span>
+                            <div class="nominal-harga">
+                                <span class="harga-pink">Rp 350K</span><span class="per-bulan">/bln</span>
+                            </div>
+                        </div>
+                        <a href="#" class="panah-pink"><svg width="16" height="16" viewBox="0 0 16 16"
+                                fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12.175 9H0V7H12.175L6.575 1.4L8 0L16 8L8 16L6.575 14.6L12.175 9V9"
+                                    fill="#E6007F" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Kartu 2: TOEFL Preparation -->
+            <div class="kartu-program">
+                <div class="bungkus-gambar">
+                    <span class="label-kategori-absolut">Preparation</span>
+                    <img src="{{ asset('images/pu2-img.jpg') }}" alt="TOEFL Preparation">
+                </div>
+                <div class="isi-kartu">
+                    <div class="header-kartu">
+                        <h3>TOEFL Preparation</h3>
+                        <span class="badge-usia"><svg width="13" height="11" viewBox="0 0 13 11"
+                                fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M6.41667 10.5L2.33333 8.28333V4.78333L0 3.5L6.41667 0L12.8333 3.5V8.16667H11.6667V4.14167L10.5 4.78333V8.28333L6.41667 10.5ZM6.41667 5.65833L10.4125 3.5L6.41667 1.34167L2.42083 3.5L6.41667 5.65833ZM6.41667 9.17292L9.33333 7.59792V5.39583L6.41667 7L3.5 5.39583V7.59792L6.41667 9.17292Z"
+                                    fill="#27171C" />
+                            </svg>
+                            Adults</span>
+                    </div>
+                    <p>Program intensif persiapan ujian TOEFL ITP/iBT untuk keperluan akademik atau karir.</p>
+                    <hr class="garis-kartu">
+                    <div class="footer-kartu">
+                        <div class="harga-mulai">
+                            <span class="label-mulai">MULAI DARI</span>
+                            <div class="nominal-harga">
+                                <span class="harga-pink">Rp 600K</span><span class="per-bulan">/bln</span>
+                            </div>
+                        </div>
+                        <a href="#" class="panah-pink"><svg width="16" height="16" viewBox="0 0 16 16"
+                                fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12.175 9H0V7H12.175L6.575 1.4L8 0L16 8L8 16L6.575 14.6L12.175 9V9"
+                                    fill="#E6007F" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Kartu 3: Bahasa Asia -->
+            <div class="kartu-program">
+                <div class="bungkus-gambar">
+                    <span class="label-kategori-absolut">B. Jepang/Korea</span>
+                    <img src="{{ asset('images/pu3-img.jpg') }}" alt="Bahasa Asia">
+                </div>
+                <div class="isi-kartu">
+                    <div class="header-kartu">
+                        <h3>Bahasa Asia</h3>
+                        <span class="badge-usia"><svg width="12" height="12" viewBox="0 0 12 12"
+                                fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M5.83333 11.6667C5.02639 11.6667 4.26806 11.5135 3.55833 11.2073C2.84861 10.901 2.23125 10.4854 1.70625 9.96042C1.18125 9.43542 0.765625 8.81806 0.459375 8.10833C0.153125 7.39861 0 6.64028 0 5.83333C0 5.02639 0.153125 4.26806 0.459375 3.55833C0.765625 2.84861 1.18125 2.23125 1.70625 1.70625C2.23125 1.18125 2.84861 0.765625 3.55833 0.459375C4.26806 0.153125 5.02639 0 5.83333 0C6.64028 0 7.39861 0.153125 8.10833 0.459375C8.81806 0.765625 9.43542 1.18125 9.96042 1.70625C10.4854 2.23125 10.901 2.84861 11.2073 3.55833C11.5135 4.26806 11.6667 5.02639 11.6667 5.83333C11.6667 6.64028 11.5135 7.39861 11.2073 8.10833C10.901 8.81806 10.4854 9.43542 9.96042 9.96042C9.43542 10.4854 8.81806 10.901 8.10833 11.2073C7.39861 11.5135 6.64028 11.6667 5.83333 11.6667ZM5.25 10.4708V9.33333C4.92917 9.33333 4.65451 9.2191 4.42604 8.99063C4.19757 8.76215 4.08333 8.4875 4.08333 8.16667V7.58333L1.28333 4.78333C1.25417 4.95833 1.22743 5.13333 1.20312 5.30833C1.17882 5.48333 1.16667 5.65833 1.16667 5.83333C1.16667 7.00972 1.55313 8.04028 2.32604 8.925C3.09896 9.80972 4.07361 10.325 5.25 10.4708ZM9.275 8.98333C9.67361 8.54583 9.97743 8.05729 10.1865 7.51771C10.3955 6.97813 10.5 6.41667 10.5 5.83333C10.5 4.88056 10.2351 4.01042 9.70521 3.22292C9.17535 2.43542 8.46806 1.86667 7.58333 1.51667V1.75C7.58333 2.07083 7.4691 2.34549 7.24062 2.57396C7.01215 2.80243 6.7375 2.91667 6.41667 2.91667H5.25V4.08333C5.25 4.24861 5.1941 4.38715 5.08229 4.49896C4.97049 4.61076 4.83194 4.66667 4.66667 4.66667H3.5V5.83333H7C7.16528 5.83333 7.30382 5.88924 7.41563 6.00104C7.52743 6.11285 7.58333 6.25139 7.58333 6.41667V8.16667H8.16667C8.41945 8.16667 8.64792 8.24201 8.85208 8.39271C9.05625 8.5434 9.19722 8.74028 9.275 8.98333Z"
+                                    fill="#27171C" />
+                            </svg>
+                            All Ages</span>
+                    </div>
+                    <p>Kelas bahasa Jepang dan Korea tingkat dasar hingga menengah bersama instruktur berpengalaman.</p>
+                    <hr class="garis-kartu">
+                    <div class="footer-kartu">
+                        <div class="harga-mulai">
+                            <span class="label-mulai">MULAI DARI</span>
+                            <div class="nominal-harga">
+                                <span class="harga-pink">Rp 450K</span><span class="per-bulan">/bln</span>
+                            </div>
+                        </div>
+                        <a href="#" class="panah-pink"><svg width="16" height="16" viewBox="0 0 16 16"
+                                fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12.175 9H0V7H12.175L6.575 1.4L8 0L16 8L8 16L6.575 14.6L12.175 9V9"
+                                    fill="#E6007F" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="aksi-bawah">
+            <a href="#" class="tombol-lihat-semua">Lihat Seluruh Program <span class="ikon-eksternal"><svg
+                        width="11" height="11" viewBox="0 0 11 11" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M1.16667 10.5C0.845833 10.5 0.571181 10.3858 0.342708 10.1573C0.114236 9.92882 0 9.65417 0 9.33333V1.16667C0 0.845833 0.114236 0.571181 0.342708 0.342708C0.571181 0.114236 0.845833 0 1.16667 0H5.25V1.16667H1.16667V9.33333H9.33333V5.25H10.5V9.33333C10.5 9.65417 10.3858 9.92882 10.1573 10.1573C9.92882 10.3858 9.65417 10.5 9.33333 10.5H1.16667ZM3.90833 7.40833L3.09167 6.59167L8.51667 1.16667H6.41667V0H10.5V4.08333H9.33333V1.98333L3.90833 7.40833Z"
+                            fill="#27171C" />
+                    </svg>
+                </span></a>
         </div>
     </section>
 
